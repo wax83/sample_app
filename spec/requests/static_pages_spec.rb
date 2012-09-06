4082d1ad6f2e9e5b957680da355c7250b3174d35
wax83 <akos.bacso@gmail.com>
2012-09-06 12:16:38 +0200

    Add title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,30 +5,36 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the h1 'Sample App'" do
-
       visit '/static_pages/home' # capibara
       page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "should have the right title" do 
-
+    it "should have the base title" do 
       visit '/static_pages/home'
-      page.should have_selector('title', :text => 'Ruby on Rails Sample App | Home')
+      page.should have_selector('title', :text => 'Ruby on Rails Sample App')
+    end
+
+    it  "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => ' | Home')
     end
   end
 
   describe "Help page" do
 
     it "should have the h1 'Help'" do
-
       visit '/static_pages/help' 
       page.should have_selector('h1', :text => 'Help')
     end
 
-    it "should have the right title" do 
-
+    it "should have the base title" do 
       visit '/static_pages/help'
-      page.should have_selector('title', :text => 'Ruby on Rails Sample App | Help')
+      page.should have_selector('title', :text => 'Ruby on Rails Sample App')
+    end
+
+    it  "should not have a custom page title" do
+      visit '/static_pages/help'
+      page.should_not have_selector('title', :text => ' | Help')
     end
   end
 
