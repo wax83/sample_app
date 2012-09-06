3ac7721f3cf6029a749145fc680ffb5338eec398
wax83 <akos.bacso@gmail.com>
2012-09-06 16:31:02 +0200

    Mess with the page titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -41,13 +41,11 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the h1 'About Us'" do
-
       visit '/static_pages/about' 
       page.should have_selector('h1', :text => 'About Us')
     end
 
     it "should have the right title" do 
-
       visit '/static_pages/about'
       page.should have_selector('title', :text => 'Ruby on Rails Sample App | About')
     end
@@ -56,13 +54,11 @@ describe "Static pages" do
   describe "Contact page" do
 
     it "should have the h1 'Contact'" do
-
       visit '/static_pages/contact' 
       page.should have_selector('h1', :text => 'Contact')
     end
 
     it "should have the right title" do 
-
       visit '/static_pages/contact'
       page.should have_selector('title', :text => 'Ruby on Rails Sample App | Contact')
     end
