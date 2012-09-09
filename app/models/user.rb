e04740f17b17cb33e20451dbc1159b570bf59554
wax83 <akos.bacso@gmail.com>
2012-09-09 18:11:55 +0200

    Change email regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 
   validates :name, :email, presence: true
   validates :name, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[\w+\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, format: { with: VALID_EMAIL_REGEX }, 
                     uniqueness: { case_sensitive: false }
   validates :password, :password_confirmation, 
