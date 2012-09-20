68c906a869b4c4df5a12b0b04f5504f2e5527791
wax83 <akos.bacso@gmail.com>
2012-09-20 12:11:22 +0200

    Add Cucumber features

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -18,12 +18,12 @@ describe "Authentication pages" do
       before { click_button "Sign in" }
 
       it { should have_selector('title', text: 'Sign in') }
-      it { should have_selector('div.alert.alert-error') }
+      it { should have_error_message }
 
       describe "after visiting another page" do
         before { click_link "Home" }
 
-        it { should_not have_selector('div.alert.alert-error') }
+        it { should_not have_error_message }
       end
     end
 
